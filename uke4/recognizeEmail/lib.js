fc4c4b4c493bee3b8a83c29538efec1b8e48936b
Stian <stiansundbyy@gmail.com>
2021-09-05 23:01:23 +0200

    IT FKN WORKS

--- uke4/recognizeEmail/lib.js
+++ uke4/recognizeEmail/lib.js
@@ -1,10 +1,40 @@
-function testEmail (email){
-    email = email.trim();
-    let seperator = email.indexOf('@');
-    let whiteSpace = email.indexOf(' ');
-    if (seperator >= 1){
-        if (whiteSpace > 0){
-            return email;
-        }
+function checkEmailAt(email){
+    if (email.includes("@")){
+        return email;
+    }
+    else {
+        return "Email must include @";
+    }
+}
+
+function checkEmailSpace(email){
+    if (!email.includes(" ")){
+        return email;
+    }
+    else {
+        return "Email can not include spaces";
+    }
+}
+
+function checkEmailPeriod(email){
+    email = getSecondPart(email);
+    if (email.includes(".")){
+        return email;
+    }
+    else {
+        return "Email does not contain a period after the @";
+    }
+
+}
+    function getSecondPart(email){
+        return email.split('@')[1];
+    }
+
+function checkEmailCorrect(email){
+    if (email=email){
+        return email;
+    }
+    else {
+        return "something has gone terribly wrong, this should not be returned."
     }
 }
