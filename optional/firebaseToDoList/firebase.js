f1ef2e29e679ad83e39374ca5db5051bbae30dcd
Stian <88629622+StianSundby@users.noreply.github.com>
2021-10-04 12:24:57 +0200

    Firebase config

---
+++ optional/firebaseToDoList/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBIOxnmJXGJ0-eE9f9bsWQ9DH96Ol2r4To",
+  authDomain: "todo-list-13377.firebaseapp.com",
+  projectId: "todo-list-13377",
+  storageBucket: "todo-list-13377.appspot.com",
+  messagingSenderId: "569856480109",
+  appId: "1:569856480109:web:8caae863f010a69e3ec064",
+  measurementId: "G-ZLS2E5F39R"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
